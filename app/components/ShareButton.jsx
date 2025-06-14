d0954f33002d06cf00ae6402d391eceeb131d29f
Sarah Hackett <sarah.hackett@flutteruki.com>
2025-06-14 15:03:48 +0100

    Implementing Search functionality

--- app/components/ShareButton.jsx
+++ app/components/ShareButton.jsx
@@ -1,10 +1,46 @@
-import { FaShare } from "react-icons/fa";
+"use client";
+
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+} from "react-share";
 
 const ShareButtons = (property) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
+
   return (
-    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      <FaShare className="fas fa-share mr-2" /> Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center -pt-2">Share This</h3>
+      <div className="flex gap-3 justify-center pb-5">
+        <FacebookShareButton
+          url={shareUrl}
+          quote={property.name}
+          hashtag={`#${property.type}`}
+        >
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+
+        <TwitterShareButton
+          url={shareUrl}
+          title={property.name}
+          hashtags={`#${property.type}`}
+        >
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+
+        <WhatsappShareButton
+          url={shareUrl}
+          title={property.name}
+          separator={""}
+        >
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+      </div>
+    </>
   );
 };
 
